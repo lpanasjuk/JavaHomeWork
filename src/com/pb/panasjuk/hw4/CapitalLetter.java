0e46dfdefa72e742551c4bde2531a1f95b3ffc1f
Ljudmila Panasjuk <Ljudmila.Panasjuk@privatbank.ua>
2021-10-30 07:49:33 +0300

    cap l3

--- src/com/pb/panasjuk/hw4/CapitalLetter.java
+++ src/com/pb/panasjuk/hw4/CapitalLetter.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 
 public class CapitalLetter {
 
-    //мой метод CapitalLetter, который возвращает слово с заглавной первой буквой
+//метод, который возвращает слово с заглавной первой буквой
     public static String CapitalLetter(String s) {
         return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
     }
 
-    //дальше метод, который принимает строку от пользователя, разбивает её и перебирает, используя метод CapitalLetter
+//ввод строки пользователем, использование метода CapitalLetter()
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите строку: ");
         String s = sc.nextLine();
+        System.out.println("Результат: ");
         String[] vsParts = s.split(" ");
         for(String sp : vsParts) {
             System.out.print(CapitalLetter(sp) + " ");
