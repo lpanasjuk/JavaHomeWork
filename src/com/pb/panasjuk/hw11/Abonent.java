f441bacaadc1b1134ba619122796014e066de40d
Ljudmila Panasjuk <Ljudmila.Panasjuk@privatbank.ua>
2021-12-17 22:54:19 +0200

    11 / 1

---
+++ src/com/pb/panasjuk/hw11/Abonent.java
@@ -0,0 +1,64 @@
+package com.pb.panasjuk.hw11;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class Abonent implements Serializable {
+    private String fullName;
+    private String dob;
+    private String phone;
+    private String address;
+    private LocalDateTime redaction;
+
+//конструктор
+    public Abonent(String fullName, String dob, String phone, String address, LocalDateTime redaction) {
+        this.fullName = fullName;
+        this.dob = dob;
+        this.phone = phone;
+        this.address = address;
+        this.redaction = redaction;
+    }
+
+//геттеры и сеттеры
+
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDateTime getRedaction() {
+        return redaction;
+    }
+
+    public void setRedaction(LocalDateTime redaction) {
+        this.redaction = redaction;
+    }
+}
