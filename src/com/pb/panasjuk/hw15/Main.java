1bc4f8c836e407b0de9839eb4710251c97886178
Ljudmila Panasjuk <Ljudmila.Panasjuk@privatbank.ua>
2022-01-16 22:16:59 +0200

    15/2

---
+++ src/com/pb/panasjuk/hw15/Main.java
@@ -0,0 +1,8 @@
+package com.pb.panasjuk.hw15;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Server server = new Server();
+    }
+}
---
+++ src/com/pb/panasjuk/hw15/MainCl.java
@@ -0,0 +1,7 @@
+package com.pb.panasjuk.hw15;
+
+public class MainCl {
+    public static void main(String[] args) {
+        ClientWindow clientWindow = new ClientWindow();
+    }
+}
